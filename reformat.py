9ca5b5d679fd4a2834a9c88e1384d84b4e7d6ef9
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-04-26 12:37:38 +0100

    Changed to use python rather than tool that no longer works

--- reformat.py
+++ reformat.py
@@ -1,11 +1,28 @@
 import argparse
+
 import sqlparse
 
 # Parse command line arguments
-parser = argparse.ArgumentParser(prog="pretty_print_sql")
+parser = argparse.ArgumentParser(prog="reformat")
 parser.add_argument("file", type=argparse.FileType("r"), nargs="+")
 args = parser.parse_args()
 
 # Pretty print input files
 for file in args.file:
-    print(sqlparse.format(file.read(), reindent=True, keyword_case='upper'))
+    original = file.read()
+    formatted = sqlparse.format(original, reindent=True, keyword_case='upper',
+                                strip_comments=False,
+                                re_indent=True,
+                                use_space_around_operators=True,
+                                indent_tabs=False,
+                                indent_width=2,
+                                comma_first=False)
+
+    if original == formatted:
+        print(file.name + ": Unchanged")
+    else:
+        print(file.name + ": Changed")
+        with open(file.name, "w") as f:
+
+            # write to file
+            f.write(formatted)
