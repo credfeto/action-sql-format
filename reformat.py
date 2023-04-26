fc9a3232653fee4fe1e12e8eed2cf37c3b496e0f
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-04-26 12:51:55 +0100

    No format just print names

--- reformat.py
+++ reformat.py
@@ -1,7 +1,5 @@
 import argparse
 
-import sqlparse
-
 # Parse command line arguments
 parser = argparse.ArgumentParser(prog="reformat")
 parser.add_argument("file", type=argparse.FileType("r"), nargs="+")
@@ -9,15 +7,16 @@ args = parser.parse_args()
 
 # Pretty print input files
 for file in args.file:
-    print("* " + file.name + ":");
+    print("* " + file.name + ":")
     original = file.read()
-    formatted = sqlparse.format(original, reindent=True, keyword_case='upper',
-                                strip_comments=False,
-                                re_indent=True,
-                                use_space_around_operators=True,
-                                indent_tabs=False,
-                                indent_width=2,
-                                comma_first=False)
+    formatted = original
+    # formatted = sqlparse.format(original, reindent=True, keyword_case='upper',
+    #                             strip_comments=False,
+    #                             re_indent=True,
+    #                             use_space_around_operators=True,
+    #                             indent_tabs=False,
+    #                             indent_width=2,
+    #                             comma_first=False)
 
     if original == formatted:
         print("  - Unchanged")
