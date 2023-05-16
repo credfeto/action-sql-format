bd1cdec03275567846702d979ac81fe10cb24d04
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-05-16 23:14:29 +0100

    attempting to fix

--- reformat.py
+++ reformat.py
@@ -1,7 +1,7 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import sys
-#import sqlparse
+import sqlparse
 
 
 def read_file(file_name):
@@ -21,17 +21,17 @@ print("Reformatting files")
 # Pretty print input files
 for file in sys.argv[1:]:
     print("* " + file + ":")
-#     original = read_file(file)
-#     formatted = sqlparse.format(original, reindent=True, keyword_case='upper',
-#                                 strip_comments=False,
-#                                 re_indent=True,
-#                                 use_space_around_operators=True,
-#                                 indent_tabs=False,
-#                                 indent_width=2,
-#                                 comma_first=False)
-#
-#     if original == formatted:
-#         print("  - Unchanged")
-#     else:
-#         print("  - Changed")
-#         write_file(file, formatted)
+    original = read_file(file)
+    formatted = sqlparse.format(original, reindent=True, keyword_case='upper',
+                                strip_comments=False,
+                                re_indent=True,
+                                use_space_around_operators=True,
+                                indent_tabs=False,
+                                indent_width=2,
+                                comma_first=False)
+
+    if original == formatted:
+        print("  - Unchanged")
+    else:
+        print("  - Changed")
+        write_file(file, formatted)
