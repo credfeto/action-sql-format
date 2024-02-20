3774f46b2551a530f62c658da4f370b71b487c28
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-02-20 09:43:31 +0000

    Spaces in filenames

---
+++ src/test/Stored Procedures/test1.sql
@@ -0,0 +1,16 @@
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [TokenPrice].[Token_Get]
+AS
+BEGIN
+  SET NOCOUNT ON;
+
+  SELECT [CoinMarketCapTokenId], [Token], [Enabled]
+  FROM TokenPrice.Token (NOLOCK)
+
+END
+GO
